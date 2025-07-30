071c7cca2488bf45527007ab67d9aa71e0e02871
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-31 00:19:20 +0530

    initiated middlewares, config and routes

--- web/backend/src/index.ts
+++ web/backend/src/index.ts
@@ -1,11 +1,31 @@
-import express, { Request, Response } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
+import cors from "cors";
 
+configDotenv();
 const app = express();
-
 const port = process.env.PORT || 3000;
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+
+// middlewares
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(express.json());
+
+app.use("/api", (req: Request, res: Response, next: NextFunction) => {
+  const ApiKey = req.headers["apikey"];
+  if (!ApiKey || ApiKey != process.env.API_KEY) res.status(401).json({ status: 401, message: "Access not allowed" })
+  else next();
+})
+
+// Routes
+app.get('/api/health', (_, res: Response) => {
+  res.status(201).send("Health Ok!");
 });
+
+mongoose.connect(`${process.env.MONGO_URL}`)
+  .then(() => console.info("Connected to Database"))
+  .catch((error) => console.error(`Cannot connect to database. Error: ${error}`))
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
