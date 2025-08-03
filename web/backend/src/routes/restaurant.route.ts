5ce9131a0cbded50a0a59cafc4981574837863b6
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-04 00:23:30 +0530

    added necessary APIs

--- web/backend/src/routes/restaurant.route.ts
+++ web/backend/src/routes/restaurant.route.ts
@@ -133,7 +133,6 @@ router.put("/forgot-password", async (req: Request, res: Response) => {
 
         res.status(202).send({
           message: "password updated successfully",
-          data
         })
       }
     }
@@ -143,4 +142,126 @@ router.put("/forgot-password", async (req: Request, res: Response) => {
     });
   }
 });
+
+// get info
+router.get('/get-info/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const restaurant = await restaurantData.findById(id, {
+      password: 0, __v: 0
+    });
+    if (!restaurant) {
+      res.status(400).send({
+        message: "cannot Find the restaurant"
+      })
+    }
+
+    res.status(200).send({
+      restaurantDetails: restaurant
+    })
+
+
+  } catch (error) {
+    res.status(400).send({
+      message: error instanceof Error ? error.message : "Unknown error occured while getting the data",
+    })
+  }
+});
+
+// update data
+router.put('/update/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const {
+      restaurantName,
+      ownerName,
+      since,
+      address,
+      phoneNumber,
+      about,
+      categories,
+    } = req.body;
+
+    // Create an object for the fields to update
+    interface dataTypes {
+      restaurantName: string;
+      ownerName: string;
+      since: string;
+      address: string;
+      phoneNumber: string;
+      about: string;
+      categories: string[];
+    }
+    const updateData: Partial<dataTypes> = {};
+
+    // Add other fields to the update object
+    if (restaurantName) updateData.restaurantName = restaurantName;
+    if (about) updateData.about = about;
+    if (categories) updateData.categories = categories;
+    if (address) updateData.address = address;
+    if (phoneNumber) updateData.phoneNumber = phoneNumber;
+    if (ownerName) updateData.ownerName = ownerName;
+    if (since) updateData.since = since;
+
+    // Update the account
+    const updatedAccount = await restaurantData.findByIdAndUpdate(
+      id,
+      updateData
+    );
+
+    // Handle case where the account does not exist
+    if (!updatedAccount) {
+      return res.status(404).send({ message: "Account not found." });
+    }
+
+    res.status(200).send({
+      message: "Account updated successfully.",
+      data: updateData,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error instanceof Error ? error.message : "Error updating account.",
+    });
+  }
+});
+
+
+// route to search restaurant via CredentailToken
+router.get('/token/:token', async (req: Request, res: Response) => {
+  const token = req.params.token;
+  try {
+    const restaurant = await restaurantData.findOne({ token: token }, { _id: 1, restaurantName: 1, token: 1 });
+
+    if (!restaurant) {
+      return res.status(400).send({
+        message: "Cannot find the restaurant"
+      });
+    }
+
+    res.status(200).send({
+      restaurant
+    });
+
+  } catch (error) {
+    res.status(400).send({
+      message: error instanceof Error ? error.message : "Cannot get info right now, please try later",
+    });
+  }
+});
+
+// Get all categories
+router.get("/get-all-categories/:restaurantId", async (req: Request, res: Response) => {
+  const { restaurantId } = req.params;
+  try {
+    const restaurant = await restaurantData.findById(restaurantId);
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+
+    res.status(200).json({ categories: restaurant.categories });
+  } catch (error) {
+    res.status(500).json({ message: error instanceof Error ? error.message : "Error retrieving categories" });
+  }
+});
+
 export default router;
