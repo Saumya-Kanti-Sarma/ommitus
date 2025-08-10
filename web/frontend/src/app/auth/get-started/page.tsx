62422f121c8e978944de0c0963f98292fc354269
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-10 16:52:04 +0530

    made show password logic

--- web/frontend/src/app/auth/get-started/page.tsx
+++ web/frontend/src/app/auth/get-started/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+
+import { useState } from "react";
+
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleTogglePassword = () => showPassword ? setShowPassword(false) : setShowPassword(true)
   return (
-    <main className="flex h-screen w-screen">
+    <main className="flex h-[100dvh] w-[100vw] max-2xl:flex-col">
       {/* Left Aside */}
-      <aside className="flex-1 bg-[var(--blue)] text-white flex flex-col justify-center items-center p-12 space-y-4">
-        <img src="/logo/ommitus-main-menu.svg" alt="logo.svg" className="w-[200px]" />
+      <aside className="flex-1 bg-[var(--blue)] text-white flex flex-col justify-center items-center p-12 space-y-4 max-md:hidden">
+        <img src="/logo/ommitus-main-menu.svg" alt="logo.svg" className=" w-[50%] max-w-[200px] " />
         <h1 className="text-9xl font-extrabold tracking-wider text-[var(--white)]">OMMITUS</h1>
         <div className="flex flex-col">
           <h3 className="text-4xl font-semibold m-0">WELCOME TO <b>OMMITUS MANAGEMENT</b></h3>
@@ -13,9 +18,10 @@ export default function Login() {
       </aside>
 
       {/* Right Form Section */}
-      <div className="flex-1 bg-[var(--white)] flex flex-col justify-center px-20 py-12 ">
-        <h1 className=" text-center text-4xl text-[var(--blue)] font-bold my-5">Register Your Restaurant With Us...</h1>
-        <form className="bg-[var(--blue)] shadow-md rounded-xl p-10 space-y-6 w-[cal(100% - 10px)]max-w-[1000px]">
+      <div className=" w-full flex-1 flex flex-col justify-center items-center px-2 py-12 max-2xl:flex-5 max-2xl:justify-start max-2xl:py-0">
+
+        <h1 className=" text-center text-4xl text-[var(--blue)] font-bold my-5 max-md:text-xl">Register Your Restaurant With <b>Ommitus</b></h1>
+        <form className="bg-[var(--blue)] shadow-md rounded-xl p-10 space-y-6 w-[98%] max-w-[1000px] max-md:p-5 max-md:space-y-3">
           {/* Restaurant Name */}
           <div>
             <label className="block mb-2 font-semibold text-[var(--white)]">Name of restaurant</label>
@@ -47,18 +53,7 @@ export default function Login() {
                 name="email"
                 placeholder="Enter the email id"
                 className="flex-1 border border-[var(--gray)] p-2 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-[var(--green)]"
-                onChange={(e) => {
-                  const btn = document.getElementById("verifyBtn");
-                  if (btn) btn.style.display = e.target.value.includes(".com") ? "block" : "none";
-                }}
               />
-              <button
-                id="verifyBtn"
-                type="button"
-                className="bg-[var(--green)] px-4 py-2 rounded-md text-white hidden"
-              >
-                Verify
-              </button>
             </div>
           </div>
 
@@ -67,13 +62,13 @@ export default function Login() {
             <label className="block mb-2 font-semibold text-[var(--white)]">Password</label>
             <div className="flex items-center border border-[var(--gray)] rounded-md bg-white">
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 placeholder="•••••"
                 className="flex-1 p-2 rounded-l-md focus:outline-none focus:ring-2 focus:ring-[var(--green)]"
               />
-              <button type="button" className="p-2">
-                <img src="/icons/eye.svg" alt="password-icon" className="w-5 h-5" />
+              <button type="button" className="p-2 cursor-pointer" onClick={handleTogglePassword}>
+                <img src={showPassword ? "/icons/eye.svg" : "/icons/close-eye.svg"} alt="password-icon" className="w-5 h-5" />
               </button>
             </div>
           </div>
@@ -81,7 +76,7 @@ export default function Login() {
           <button className="bg-white w-full p-3 rounded-md">Create Account</button>
         </form>
         <p className="text-center mt-1">Already have Account? <button className="text-[var(--red)]"> click here to Login</button></p>
-        <button className="w-[cal(100% - 10px)]max-w-[1000px] p-4 rounded-2xl bg-[var(--blue)] text-white mt-10 text-2xl font-medium">Watch a tutorial</button>
+        <button className="w-[96%] max-w-[1000px] p-4 rounded-2xl bg-[var(--blue)] text-white mt-10 text-2xl font-medium">Watch a tutorial</button>
       </div>
     </main>
   );
