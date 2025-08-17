5479b0f6029b065f805e374727b97c512f583be2
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-17 23:46:02 +0530

    added key ref and ref function

--- web/frontend/src/app/auth/get-started/page.tsx
+++ web/frontend/src/app/auth/get-started/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Cookies from "js-cookie"; // Install: npm install js-cookie
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
@@ -7,6 +7,12 @@ import toast from "react-hot-toast";
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
+
+  const resRef = useRef<HTMLInputElement>(null);
+  const ownerNameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const actionBtndRef = useRef<HTMLButtonElement>(null); // create acount / login btn
 
   const router = useRouter();
 
@@ -41,6 +47,22 @@ export default function Login() {
 
   };
 
+  const handleKeyDown = (
+    nextRef: React.RefObject<HTMLInputElement | HTMLButtonElement | null>,
+    prevRef: React.RefObject<HTMLInputElement | HTMLButtonElement | null>,
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    console.log(event.key);
+
+    if (event.key === "Enter" || event.key === "ArrowDown") {
+      event.preventDefault()
+      nextRef.current?.focus()
+    } else if (event.key === "ArrowUp") {
+      event.preventDefault();
+      prevRef.current?.focus();
+    };
+  }
+
   return (
     <main className="flex h-[100dvh] w-[100vw] max-2xl:flex-col">
       {/* Left Aside */}
@@ -48,13 +70,13 @@ export default function Login() {
         <img
           src="/logo/ommitus-main-menu.svg"
           alt="logo.svg"
-          className=" w-[50%] max-w-[200px] "
+          className=" w-[50%] max-w-[200px] max-lg:w-[120px]"
         />
-        <h1 className="text-9xl font-extrabold tracking-wider text-[var(--white)]">
+        <h1 className="text-9xl font-extrabold tracking-wider text-[var(--white)] max-lg:text-6xl">
           OMMITUS
         </h1>
         <div className="flex flex-col">
-          <h3 className="text-4xl font-semibold m-0">
+          <h3 className="text-4xl font-semibold m-0 max-lg:text-2xl">
             WELCOME TO <b>OMMITUS MANAGEMENT</b>
           </h3>
           <p className="text-4xl italic m-0">
@@ -86,6 +108,8 @@ export default function Login() {
               name="restaurantName"
               placeholder="Enter the name of your restaurant"
               className="w-full border border-[var(--gray)] p-2 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-[var(--green)]"
+              ref={resRef}
+              onKeyDown={(e) => handleKeyDown(ownerNameRef, actionBtndRef, e)}
             />
           </div>
 
@@ -98,8 +122,10 @@ export default function Login() {
               <input
                 type="text"
                 name="ownerName"
+                ref={ownerNameRef}
                 placeholder="Enter the name of restaurant owner"
                 className="w-full border border-[var(--gray)] p-2 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-[var(--green)]"
+                onKeyDown={(e) => handleKeyDown(emailRef, resRef, e)}
               />
             </div>
           )}
@@ -114,8 +140,10 @@ export default function Login() {
                 <input
                   type="email"
                   name="email"
+                  ref={emailRef}
                   placeholder="Enter the email id"
                   className="flex-1 border border-[var(--gray)] p-2 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-[var(--green)]"
+                  onKeyDown={(e) => handleKeyDown(passwordRef, ownerNameRef, e)}
                 />
               </div>
             </div>
@@ -130,8 +158,10 @@ export default function Login() {
               <input
                 type={showPassword ? "text" : "password"}
                 name="password"
+                ref={passwordRef}
                 placeholder="•••••"
                 className="flex-1 p-2 rounded-l-md focus:outline-none focus:ring-2 focus:ring-[var(--green)]"
+                onKeyDown={(e) => handleKeyDown(actionBtndRef, emailRef, e)}
               />
               <button
                 type="button"
@@ -152,7 +182,23 @@ export default function Login() {
           </div>
 
           <br />
-          <button type="submit" className="bg-white w-full p-3 rounded-md">
+
+          {/* action button (Create account / login account) */}
+          <button
+            type="submit"
+            className="bg-white w-full p-3 rounded-md"
+            ref={actionBtndRef}
+            onKeyDown={(e) => {
+              if (e.key == "ArrowUp") {
+                e.preventDefault();
+                passwordRef.current?.focus();
+              };
+              if (e.key == "ArrowDown") {
+                e.preventDefault();
+                resRef.current?.focus();
+              }
+            }}
+          >
             {isLogin ? "Login" : "Create Account"}
           </button>
         </form>
