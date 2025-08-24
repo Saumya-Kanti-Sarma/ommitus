f7c43f850fcfe88f0cfb704166f5489903beabc2
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-24 10:31:10 +0530

    added functionalities to add menu page

--- web/frontend/src/app/restaurant/[id]/create/page.tsx
+++ web/frontend/src/app/restaurant/[id]/create/page.tsx
@@ -1,10 +1,64 @@
 "use client";
-import { useState } from "react";
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import toast from "react-hot-toast";
+
 
 export default function DishForm() {
-  const [image, setImage] = useState<File | null>(null);
-  const [preview, setPreview] = useState<string | null>(null);
+  const [image, setImage] = useState<File | null>(null); // this stores the image file
+  const [preview, setPreview] = useState<string | null>(null); // this stores the url of image  to preview it before uploading and to show on screen
+  const [categories, setCategories] = useState<string[]>([]);// lists of all categories i.e fetched from server
+  const [formData, setFromData] = useState({
+    dishName: "",
+    image: "",
+    category: "",
+    veg: false,
+    description: "",
+    fullPlate: "",
+    halfPlate: "",
+    available: true,
+    restaurantId: "",
+  })
 
+  // API keys
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+  //Cookies
+  const restaurantId = Cookies.get("restaurantId");
+  useEffect(() => {
+    const restaurantId = Cookies.get("restaurantId");
+    if (restaurantId) {
+      setFromData((prev) => ({ ...prev, restaurantId }));
+    }
+  }, [])
+
+  // load categories
+  useEffect(() => {
+    const fetchRestaurantInfo = async () => {
+      try {
+        const res = await axios.get(
+          `${API_URL}/api/restaurant/get-all-categories/${restaurantId}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              xkc: API_KEY!,
+            },
+          }
+        );
+        const data = res.data;
+        // console.log(data.categories);
+        setCategories(data.categories);
+      }
+      catch (err) {
+        console.error("Error fetching restaurant info:", err);
+      }
+    };
+    fetchRestaurantInfo();
+  }, [])
+
+  // load the file and preview image befire uploading
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
@@ -13,27 +67,86 @@ export default function DishForm() {
     }
   };
 
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+  // handle form change
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      const file = e.dataTransfer.files[0];
-      setImage(file);
-      setPreview(URL.createObjectURL(file));
+    const { name, value } = e.target;
+    if (name == "veg") {
+      let VegValue;
+      value == "Yes" ? VegValue = true : VegValue = false;
+      setFromData(prev => ({ ...prev, veg: VegValue }))
+      return;
+    };
+    if (name == "available") {
+      let availablevalue;
+      value == "Available" ? availablevalue = true : availablevalue = false;
+      setFromData(prev => ({ ...prev, veg: availablevalue }))
+      return;
+    };
+    setFromData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+    // console.log(formData);
+
+  }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const loading = toast.loading("Uploading menu...");
+    console.log("Submitted");
+
+    try {
+      let uploadedImageUrl = "";
+
+      // 1. Upload image to Supabase
+      if (image) {
+        const formData = new FormData();
+        formData.append("file", image);
+
+        const uploadRes = await axios.post("/api/upload-img", formData);
+        uploadedImageUrl = uploadRes.data.url;
+
+        setFromData((prev) => ({ ...prev, image: uploadedImageUrl }));
+      }
+
+      // 2. Post formData to your API
+      const res = await axios.post(
+        `${API_URL}/api/menu/add-menu`,
+        { ...formData, image: uploadedImageUrl }, // send JSON
+        {
+          headers: {
+            "Content-Type": "application/json",
+            xkc: API_KEY,
+            xrid: restaurantId,
+          },
+        }
+      );
+
+      toast.success(res.data.message || "Menu added successfully");
+      console.log(res.data);
+
+    } catch (err: any) {
+      if (axios.isAxiosError(err)) {
+        console.error("Axios error:", err.response?.data || err.message);
+        toast.error(err.response?.data?.error || "Request failed");
+      } else {
+        // Non-Axios error
+        console.error("Unexpected error:", err);
+        toast.error("Something went wrong");
+      }
+    } finally {
+      toast.dismiss(loading);
     }
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Submitted");
-  };
 
   return (
-    <div className="min-h-[calc(100vh-70px)] w-full  flex items-center justify-center bg-[var(--light-gray)] max-md:min-h-[calc(100vh-60px)]">
+    <div className="min-h-[calc(100vh-70px)] w-full  flex items-center justify-center bg-[var(--white)] max-md:min-h-[calc(100vh-60px)]">
       <form
         onSubmit={handleSubmit}
-        className="bg-[var(--white)]  rounded-2xl shadow-lg w-[98%] max-w-[1080px] p-10"
+        className="bg-[var(--white)]  rounded-2xl shadow-2xl w-[98%] max-w-[1080px] p-10"
       >
-        <h1 className="text-2xl font-bold text-[var(--blue)] mb-4">Add Dish</h1>
+        <h1 className="text-2xl font-bold text-[var(--blue)] mb-4">Add Dish To Menu</h1>
 
         {/* Dish Name */}
         <label className="block mb-2 text-[var(--dark-blue)] font-semibold">
@@ -43,6 +156,7 @@ export default function DishForm() {
           type="text"
           name="dishName"
           placeholder="Enter dish name"
+          onChange={handleOnChange}
           className="w-full border border-[var(--gray)] p-2 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-[var(--blue)]"
           required
         />
@@ -51,21 +165,22 @@ export default function DishForm() {
         <label className="block mb-2 text-[var(--dark-blue)] font-semibold">
           Dish Image
         </label>
-        <div
-          onDrop={handleDrop}
-          onDragOver={(e) => e.preventDefault()}
-          className="border-2 border-dashed border-[var(--blue)] rounded-lg p-4 text-center mb-4 cursor-pointer"
-        >
+        <div className="border-2 border-dashed border-[var(--blue)] rounded-lg p-4 text-center mb-4 cursor-pointer relative w-full">
           <input
             type="file"
             accept="image/*"
-            onChange={handleImageChange}
-            className="hidden"
+            className="bg-orange-400 absolute w-[100%] left-0 top-0 h-full opacity-0"
             id="imageUpload"
+            onChange={handleImageChange}
           />
-          <label htmlFor="imageUpload" className="cursor-pointer">
-            Drag & Drop or Click to Upload
-          </label>
+          {!preview && (
+            <>
+              <label htmlFor="imageUpload" className="cursor-pointer">
+                Drag & Drop or Click to Upload
+              </label>
+            </>
+          )}
+
           {preview && (
             <img
               src={preview}
@@ -80,11 +195,12 @@ export default function DishForm() {
           Category
         </label>
         <div className="flex gap-4 mb-4">
-          {["Starter", "Main Course", "Beverage"].map((cat) => (
+          {categories && categories.length > 0 && categories.map((cat) => (
             <label key={cat} className="flex items-center gap-2">
               <input
                 type="radio"
                 name="category"
+                onChange={handleOnChange}
                 value={cat}
                 defaultChecked={cat === "Starter"}
               />
@@ -99,11 +215,11 @@ export default function DishForm() {
         </label>
         <div className="flex gap-4 mb-4">
           <label className="flex items-center gap-2">
-            <input type="radio" name="isVeg" value="Yes" defaultChecked />
+            <input type="radio" name="veg" value="Yes" onChange={handleOnChange} />
             Yes
           </label>
           <label className="flex items-center gap-2">
-            <input type="radio" name="isVeg" value="No" />
+            <input type="radio" name="veg" value="No" onChange={handleOnChange} defaultChecked />
             No
           </label>
         </div>
@@ -112,11 +228,11 @@ export default function DishForm() {
         <label className="block mb-2 text-[var(--dark-blue)] font-semibold">
           Description
         </label>
-        <textarea
+        <input
           name="description"
           placeholder="Enter description"
+          onChange={handleOnChange}
           className="w-full border border-[var(--gray)] p-2 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-[var(--blue)]"
-          rows={4}
         />
 
         {/* Prices */}
@@ -127,8 +243,9 @@ export default function DishForm() {
             </label>
             <input
               type="number"
-              name="fullPrice"
+              name="fullPlate"
               placeholder="₹ Full price"
+              onChange={handleOnChange}
               className="w-full border border-[var(--gray)] p-2 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-[var(--blue)]"
               required
             />
@@ -139,12 +256,26 @@ export default function DishForm() {
             </label>
             <input
               type="number"
-              name="halfPrice"
+              name="halfPlate"
+              onChange={handleOnChange}
               placeholder="₹ Half price"
               className="w-full border border-[var(--gray)] p-2 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-[var(--blue)]"
-              required
             />
           </div>
+        </div>
+        {/* Available? */}
+        <label className="block mb-2 text-[var(--dark-blue)] font-semibold">
+          Available
+        </label>
+        <div className="flex gap-4 mb-4">
+          <label className="flex items-center gap-2">
+            <input type="radio" name="available" value="Available" onChange={handleOnChange} defaultChecked />
+            Available
+          </label>
+          <label className="flex items-center gap-2">
+            <input type="radio" name="available" value="Un-available" onChange={handleOnChange} />
+            Un-available
+          </label>
         </div>
 
         {/* Submit Button */}
