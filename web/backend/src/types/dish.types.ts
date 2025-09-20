4c60ce7e07835adbc33bc90490b4d686570b586c
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-09-20 17:35:09 +0530

    added types

---
+++ web/backend/src/types/dish.types.ts
@@ -0,0 +1,12 @@
+export default interface DishTypes {
+  dishName?: string;
+  image?: string;
+  category?: string;
+  veg?: string;
+  description?: string;
+  fullPlate?: number;
+  halfPlate?: number;
+  available?: boolean;
+  restaurantId?: string;
+  createdAt?: string;
+}
---
+++ web/backend/src/types/restaurant.types.ts
@@ -0,0 +1,12 @@
+export default interface RestaurantTypes {
+  restaurantName?: string;
+  email?: string;
+  password?: string;
+  ownerName?: string;
+  since?: string;
+  address?: string;
+  phoneNumber?: string;
+  token?: string;
+  created?: string;
+  categories?: string[];
+}
