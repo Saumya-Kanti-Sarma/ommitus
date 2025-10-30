1c73bff4b35f047a5d2dfc1ad760d233dae7f58e
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-10-30 23:50:59 +0530

    updated landing page

--- web/frontend/src/app/page.tsx
+++ web/frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
-import LandingNavbar from "@/components/UI/Landing/Landing.Navbar";
+"use client";
+import LandingNavbar from "@/components/UI/Landing/Navbar.landing";
 import LandingSideBar from "@/components/UI/Landing/Sidebar.landing";
+import { useState } from "react";
 
 export default function Home() {
+  const [sidebar, setSideBar] = useState(false);
   return (
     <main className="relative">
-      <LandingNavbar />
-      <LandingSideBar />
+      <LandingNavbar handleNavbtnClick={() => setSideBar((prev) => !prev)} />
+      <LandingSideBar showSideBar={sidebar} />
     </main>
   );
 }
