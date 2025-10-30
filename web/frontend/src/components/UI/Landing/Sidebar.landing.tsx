029dbf45ec7fd8dee2769b2229af710bad9e3c26
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-10-30 23:51:47 +0530

    added toggling functionality

--- web/frontend/src/components/UI/Landing/Sidebar.landing.tsx
+++ web/frontend/src/components/UI/Landing/Sidebar.landing.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+"use client"
+import { SidebarTypes } from '@/types/Landing.types'
+const LandingSideBar = ({ showSideBar }: SidebarTypes) => {
 
-const LandingSideBar = () => {
   return (
-    <div className='bg-red-400 h-[calc(100vh-70px)] text-white '>
+    <div className={`
+      bg-[var(--blue)] h-[calc(100vh-70px)] text-white w-full max-w-[360px] 
+      absolute transition-transform duration-300 ease-in-out
+      ${showSideBar ? 'translate-x-0' : 'translate-x-[-100%]'}
+    `}>
       this is sidebarzzzz
     </div>
   )
