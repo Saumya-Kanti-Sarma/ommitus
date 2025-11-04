ce80edadd25c52c66851cea5c6c9fefc0fb4248d
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-11-04 19:56:04 +0530

    added link hover animation

---
+++ web/frontend/src/components/UI/Landing/Navbar/Navbar.landing.tsx
@@ -0,0 +1,54 @@
+"use client";
+import "./Navbar.landing.css";
+import Link from "next/link";
+import { LandingPageTypes } from "@/types/Landing.types";
+import { useState } from "react";
+import TypingAnimation from "../../Typing-Animation/TypingAnimation.UI.Component";
+const LandingNavbar = ({ handleNavbtnClick = () => { } }: LandingPageTypes) => {
+  const [navImg, setNavImg] = useState("/icons/menu.svg");
+  return (
+    <nav className="h-[70px] bg-[var(--black)] text-white py-4 px-8 flex justify-between items-center shadow-md relative border-b-1 border-[#ffffff42]">
+      {/* Menu Button for Mobile */}
+      <button className="block md:hidden cursor-pointer" onClick={() => {
+        handleNavbtnClick();
+        setNavImg((prev) => prev == "/icons/menu.svg" ? "/icons/gray-menu.svg" : "/icons/menu.svg")
+      }}>
+        <img src={navImg} alt="menu" className="w-8" />
+      </button>
+      {/* Brand */}
+      <Link href={"/"}>
+        <div className="flex justify-start items-center gap-1">
+          <img src="/logo/ommitus-1.svg" alt="menu-logo" className="max-w-[50px] mr-2 nav-img" />
+          <div className="flex flex-col justify-start items-start">
+            <TypingAnimation text="OMMITUS" extraStyles="text-[20px] font-black m-0" />
+            <TypingAnimation text="Restaurant Services" extraStyles="text-[14px]" />
+          </div>
+        </div>
+      </Link>
+      {/* Navigation Links */}
+      <div className="flex justify-between items-center gap-6">
+        <ul className="hidden md:flex items-center gap-6">
+          {['Features', 'About', 'Pricing', 'FAQs'].map((item, index) => (
+            <li className="landing-nav-hover cursor-pointer" key={index}>{item}</li>
+          ))}
+          <Link
+            target="blank"
+            href="/auth/get-started"
+            className="landing-nav-hover cursor-pointer"
+          >
+            Login
+          </Link>
+        </ul>
+        <Link
+          target="blank"
+          href="/auth/get-started"
+          className="bg-white text-[var(--black)] px-4 py-1 rounded-md font-medium hover:opacity-90 transition-all"
+        >
+          Get Started
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default LandingNavbar;
