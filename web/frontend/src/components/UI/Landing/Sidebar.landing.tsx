6a9aeeec43230ad241c4f4c5c008b0c5fda74751
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-11-09 17:45:35 +0530

    change bg

--- web/frontend/src/components/UI/Landing/Sidebar.landing.tsx
+++ web/frontend/src/components/UI/Landing/Sidebar.landing.tsx
@@ -4,7 +4,7 @@ const LandingSideBar = ({ showSideBar }: SidebarTypes) => {
 
   return (
     <div className={`
-      bg-[var(--blue)] h-[calc(100vh-70px)] text-white w-full max-w-[360px] 
+      bg-[#131313] h-[calc(100vh-70px)] text-white w-full max-w-[360px] 
       absolute transition-transform duration-300 ease-in-out
       ${showSideBar ? 'translate-x-0' : 'translate-x-[-100%]'}
     `}>
