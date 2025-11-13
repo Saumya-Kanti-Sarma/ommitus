c3b92252bd02708cd379e0917e75e1feac6795bc
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-11-13 13:06:32 +0530

    media quieries

--- web/frontend/src/components/UI/Landing/Main/MainLanding.tsx
+++ web/frontend/src/components/UI/Landing/Main/MainLanding.tsx
@@ -10,11 +10,11 @@ const MainLanding = () => {
 
         <div className="  w-full max-w-7xl h-[calc(100vh-70px)] flex flex-col justify-center items-center gap-0 ">
           <div className="dotted-div flex-col">
-            <img src="/logo/ommitus-logo.svg" alt="ommitus-logo" className="w-[200px]" />
-            <h1 className="text-8xl font-black">OMMITUS</h1>
+            <img src="/logo/ommitus-logo.svg" alt="ommitus-logo" className="w-[200px] max-md:w-[150px] max-sm:w-[100px]" />
+            <h1 className="text-8xl font-black max-md:text-6xl max-sm:text-4xl">OMMITUS</h1>
           </div>
           <div className="dotted-div">
-            <h1 className="text-4xl font-bold w-fit">
+            <h1 className="text-4xl font-bold w-fit text-center max-md:text-2xl max-sm:text-xl">
               We provide{" "}
               <span className="font-black digital-smart">
                 Digitally Smart Menu
@@ -22,10 +22,10 @@ const MainLanding = () => {
               for your restaurant
             </h1>
           </div>
-          <p className="text-2xl max-w-4xl text-center"> Let the experience thrive with our digital menu that can be shared on google reviews, social medias, posters, banners anywhere any place...</p>
-          <div className="flex flex-col sm:flex-row items-center justify-between  mt-6  w-full max-w-3xl">
+          <p className="text-2xl max-w-4xl text-center max-md:text-m max-sm:text-[14px] max-sm:max-w-[380px]"> Let the experience thrive with our digital menu that can be shared on google reviews, social medias, posters, banners anywhere any place...</p>
+          <div className="flex flex-col sm:flex-row items-center justify-between  mt-6  w-full max-w-3xl max-md:justify-center max-md:gap-5">
             <GetStartedBtn fontSize="30px" borderRadius="10px" width="300px" />
-            <Link href="/" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
+            <Link href="/" target="_blank" rel="noopener noreferrer" className="text-white underline">
               Watch customer success story
             </Link>
           </div>
